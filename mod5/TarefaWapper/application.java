2e64e5c7dd855cfd1bb9e6c192a83c759b5ab5de
RegisDev777 <regisbale289@gmail.com>
2024-09-02 02:19:38 -0300

    Tarefa WrapperClasses

---
+++ mod5/TarefaWapper/application.java
@@ -0,0 +1,20 @@
+package testCodigo;
+
+import java.util.Scanner;
+
+public class application {
+       public static void main(String[] args) {
+		
+      
+    	   Scanner sc = new Scanner(System.in);
+    	   
+    	   
+    	   System.out.println("Num a:");
+    	    double a = sc.nextDouble();
+    	    
+    	    Double aWrapper = a;
+    	    
+    	    System.out.println(a);
+    	   
+	}
+}
